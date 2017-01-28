202c8ef6b7966524c9bf29c461f5ec3a7025abe0
Nelson Melecio <ntm100986@gmail.com>
2017-01-29 03:17:26 +0800

    Added new layout to be use for all admin pages.

---
+++ resources/views/vendor/adminlte/layouts/sge_layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+@section('htmlheader')
+    @include('adminlte::layouts.partials.htmlheader')
+@show
+
+<body class="skin-blue sidebar-mini">
+
+    <div id="app">
+
+        <div class="wrapper">
+
+            @include('adminlte::layouts.partials.mainheader')
+            @include('adminlte::layouts.partials.sidebar')
+
+            <div class="content-wrapper">
+                <section class="content" style="min-height: 768px; !important;">
+                    @yield('content-header-v2')
+                    @yield('main-content')
+                </section>
+            
+            </div>
+
+            @include('adminlte::layouts.partials.controlsidebar')
+
+            @include('adminlte::layouts.partials.footer')
+
+        </div>
+
+    </div>
+
+    @section('scripts')
+        @include('adminlte::layouts.partials.scripts')
+    @show
+
+</body>
+</html>
