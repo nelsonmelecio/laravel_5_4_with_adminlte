36158952529a5695fe9b57d0ce08b57afc517c91
Nelson Melecio <ntm100986@gmail.com>
2017-01-29 03:17:57 +0800

    Updated routes for Risk CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,20 @@ Route::post('/inquiry', 'InquiryController@inquiry');
 
 Route::group(['middleware' => 'auth'], function () {
 
+
+	//  RISK Routes 
 	Route::get('/risks', 'RiskController@index');
+
+	Route::get('/risk/add', 'RiskController@getAddRiskView');
+	Route::post('/risk/add', 'RiskController@postAddRiskView');
+
+	Route::get('/risk/edit/{id}', 'RiskController@getEditRiskView');
+	Route::post('/risk/update', 'RiskController@postEditRiskView');
+
+	Route::get('/risk/delete/{id}', 'RiskController@getDeleteRiskView');
+	Route::post('/risk/delete', 'RiskController@postDeleteRiskView');
+
+
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
