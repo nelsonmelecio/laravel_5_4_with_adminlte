55b9b1b645ebf847e759a0d06d35822f526bcbcb
Nelson Melecio <ntm100986@gmail.com>
2017-01-30 00:02:58 +0800

    Added new macro views for form related stuff.

---
+++ resources/views/macro/text_disabled.blade.php
@@ -0,0 +1,6 @@
+<!-- call include('macro.text_disabled', 0_label, 1_field, 2_value, 3_placeholder ) -->
+
+<div class="form-group">
+	<label for="{{ $data[1] }}">{{ $data[0] }}</label>
+	<input class="form-control" name="{{ $data[1] }}" placeholder="{{ $data[1] }}" type="text" value="{{ $data[2] }}"  style="color: #000 !important;" disabled>
+</div>
